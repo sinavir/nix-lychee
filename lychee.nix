0a733ce9ca8faf02b5a6dfc864bc4525affb4eb6
sinavir <sinavir@sinavir.fr>
2022-12-12 05:11:12 +0100

    ~

--- lychee.nix
+++ lychee.nix
@@ -13,160 +13,163 @@ in
       example = "www.example.com";
     };
     forceSSL = lib.mkOption {
-      types = lib.types.bool;
+      type = lib.types.bool;
       default = true;
       description = "Whether to force SSL for the nginx virtual host";
     };
     enableACME = lib.mkOption {
-      types = lib.types.bool;
+      type = lib.types.bool;
       default = true;
       description = "Whether to enableACME for the nginx virtual host";
     };
     upload_max_filesize = lib.mkOption {
-      types = lib.types.ints.positive;
+      type = lib.types.ints.positive;
       default = 30;
       description = "Max uploaded file size";
     };
     post_max_size = lib.mkOption {
-      types = lib.types.ints.positive;
+      type = lib.types.ints.positive;
       default = 100;
       description = "Max post request size";
     };
     user = lib.mkOption {
-      types = lib.types.str;
+      type = lib.types.str;
       default = "lychee";
       description = "The user that will operate on mutable files";
     };
     stateDirectory = lib.mkOption {
-      types = lib.types.path;
-      default = "/var/lib/lychee/";
+      type = lib.types.path;
+      default = "/var/lib/lychee";
     };
     settings = lib.mkOption {
-      freefromType = with lib.types; attrsOf  str;
-
-      options = {
-        "DB_DATABASE"= lib.mkOption {
-          type = lib.types.str;
-          default = "${cfg.stateDirectory}/db.sqlite";
-        };
-        "APP_NAME"= lib.mkOption {
-          type = lib.types.str;
-          default = "Lychee";
-        };
-        "APP_ENV"= lib.mkOption {
-          type = lib.types.str;
-          default = "production";
-        };
-        "APP_DEBUG"= lib.mkOption {
-          type = lib.types.str;
-          default = "false";
-        };
-        "APP_URL"= lib.mkOption {
-          type = lib.types.str;
-          default = "http://localhost";
-        };
-        "DEBUGBAR_ENABLED"= lib.mkOption {
-          type = lib.types.str;
-          default = "false";
-        };
-        "DB_OLD_LYCHEE_PREFIX"= lib.mkOption {
-          type = lib.types.str;
-          default = "";
-        };
-        "DB_CONNECTION"= lib.mkOption {
-          type = lib.types.str;
-          default = "sqlite";
-        };
-        "DB_HOST"= lib.mkOption {
-          type = lib.types.str;
-          default = "";
-        };
-        "DB_PORT"= lib.mkOption {
-          type = lib.types.str;
-          default = "";
-        };
-        "DB_USERNAME"= lib.mkOption {
-          type = lib.types.str;
-          default = "";
-        };
-        "DB_PASSWORD"= lib.mkOption {
-          type = lib.types.str;
-          default = "";
-        };
-        "DB_LOG_SQL"= lib.mkOption {
-          type = lib.types.str;
-          default = "false";
-        };
-        "LYCHEE_UPLOADS" = lib.mkOption {
-          type = lib.type.path;
-          default = "${cfg.stateDirectory}/public/uploads";
-        };
-        "CACHE_DRIVER"= lib.mkOption {
-          type = lib.types.str;
-          default = "file";
-        };
-        "SESSION_DRIVER"= lib.mkOption {
-          type = lib.types.str;
-          default = "file";
-        };
-        "SESSION_LIFETIME"= lib.mkOption {
-          type = lib.types.str;
-          default = "120";
-        };
-        "SECURITY_HEADER_HSTS_ENABLE"= lib.mkOption {
-          type = lib.types.str;
-          default = "false";
-        };
-        "SESSION_SECURE_COOKIE"= lib.mkOption {
-          type = lib.types.str;
-          default = "false";
-        };
-        "REDIS_HOST"= lib.mkOption {
-          type = lib.types.str;
-        };
-        "REDIS_PASSWORD"= lib.mkOption {
-          type = lib.types.str;
-          default = "null";
-        };
-        "REDIS_PORT"= lib.mkOption {
-          type = lib.types.str;
-          default = "6379";
-        };
-        "MAIL_DRIVER"= lib.mkOption {
-          type = lib.types.str;
-          default = "smtp";
-        };
-        "MAIL_HOST"= lib.mkOption {
-          type = lib.types.str;
-          default = "";
-        };
-        "MAIL_PORT"= lib.mkOption {
-          type = lib.types.str;
-          default = "";
-        };
-        "MAIL_USERNAME"= lib.mkOption {
-          type = lib.types.str;
-          default = "";
-        };
-        "MAIL_PASSWORD"= lib.mkOption {
-          type = lib.types.str;
-          default = "";
-        };
-        "MAIL_ENCRYPTION"= lib.mkOption {
-          type = lib.types.str;
-          default = "";
-        };
-        "MAIL_FROM_NAME"= lib.mkOption {
-          type = lib.types.str;
-          default = "";
-        };
-        "MAIL_FROM_ADDRESS"= lib.mkOption {
-          type = lib.types.str;
-          default = "";
-        };
-        "TRUSTED_PROXIES"= lib.mkOption {
-          type = lib.types.str;
-          default = "null";
+      default = {};
+      type = lib.types.submodule {
+        freeformType = with lib.types; attrsOf str;
+        options = {
+          DB_DATABASE= lib.mkOption {
+            type = lib.types.str;
+            default = "${cfg.stateDirectory}/db.sqlite";
+          };
+          APP_NAME= lib.mkOption {
+            type = lib.types.str;
+            default = "Lychee";
+          };
+          APP_ENV = lib.mkOption {
+            type = lib.types.str;
+            default = "production";
+          };
+          APP_DEBUG = lib.mkOption {
+            type = lib.types.str;
+            default = "\"false\"";
+          };
+          APP_URL = lib.mkOption {
+            type = lib.types.str;
+            default = "http://localhost";
+          };
+          DEBUGBAR_ENABLED = lib.mkOption {
+            type = lib.types.str;
+            default = "\"false\"";
+          };
+          #DB_OLD_LYCHEE_PREFIX = lib.mkOption {
+          #  type = lib.types.str;
+          #  default = "";
+          #};
+          DB_CONNECTION = lib.mkOption {
+            type = lib.types.str;
+            default = "sqlite";
+          };
+          #DB_HOST = lib.mkOption {
+          #  type = lib.types.str;
+          #  default = "";
+          #};
+          #DB_PORT = lib.mkOption {
+          #  type = lib.types.str;
+          #  default = "";
+          #};
+          #DB_USERNAME = lib.mkOption {
+          #  type = lib.types.str;
+          #  default = "";
+          #};
+          #DB_PASSWORD = lib.mkOption {
+          #  type = lib.types.str;
+          #  default = "";
+          #};
+          DB_LOG_SQL = lib.mkOption {
+            type = lib.types.str;
+            default = "\"false\"";
+          };
+          LYCHEE_UPLOADS = lib.mkOption {
+            type = lib.types.path;
+            default = "${cfg.stateDirectory}/uploads";
+          };
+          CACHE_DRIVER = lib.mkOption {
+            type = lib.types.str;
+            default = "file";
+          };
+          SESSION_DRIVER = lib.mkOption {
+            type = lib.types.str;
+            default = "file";
+          };
+          SESSION_LIFETIME = lib.mkOption {
+            type = lib.types.str;
+            default = "120";
+          };
+          SECURITY_HEADER_HSTS_ENABLE = lib.mkOption {
+            type = lib.types.str;
+            default = "\"false\"";
+          };
+          SESSION_SECURE_COOKIE = lib.mkOption {
+            type = lib.types.str;
+            default = "\"false\"";
+          };
+          #REDIS_HOST = lib.mkOption {
+          #  type = lib.types.str;
+          #  default = "";
+          #};
+          REDIS_PASSWORD = lib.mkOption {
+            type = lib.types.str;
+            default = "\"null\"";
+          };
+          REDIS_PORT = lib.mkOption {
+            type = lib.types.str;
+            default = "6379";
+          };
+          MAIL_DRIVER = lib.mkOption {
+            type = lib.types.str;
+            default = "smtp";
+          };
+          #MAIL_HOST = lib.mkOption {
+          #  type = lib.types.str;
+          #  default = "";
+          #};
+          #MAIL_PORT = lib.mkOption {
+          #  type = lib.types.str;
+          #  default = "";
+          #};
+          #MAIL_USERNAME = lib.mkOption {
+          #  type = lib.types.str;
+          #  default = "";
+          #};
+          #MAIL_PASSWORD = lib.mkOption {
+          #  type = lib.types.str;
+          #  default = "";
+          #};
+          #MAIL_ENCRYPTION = lib.mkOption {
+          #  type = lib.types.str;
+          #  default = "";
+          #};
+          #MAIL_FROM_NAME = lib.mkOption {
+          #  type = lib.types.str;
+          #  default = "";
+          #};
+          #MAIL_FROM_ADDRESS = lib.mkOption {
+          #  type = lib.types.str;
+          #  default = "";
+          #};
+          TRUSTED_PROXIES = lib.mkOption {
+            type = lib.types.str;
+            default = "\"null\"";
+          };
         };
       };
     };
@@ -175,7 +178,7 @@ in
     services.nginx = {
       enable = true;
       virtualHosts.${cfg.website} = {
-        root = cfg.stateDirectory + "/public";
+        root = cfg.stateDirectory + "/www/public/";
         forceSSL = lib.mkDefault cfg.forceSSL;
         enableACME = lib.mkDefault cfg.enableACME;
         locations = {
@@ -185,15 +188,15 @@ in
             };
             extraConfig = ''
               fastcgi_split_path_info ^(.+?\.php)(/.*)$;
-              fastcgi_pass unix:${config.services.phpfpm.pools."${config.website}".socket};
+              fastcgi_pass unix:${config.services.phpfpm.pools."${cfg.website}".socket};
               fastcgi_index index.php;
-              client_max_body_size ${cfg.upload_max_filesize}M;
+              client_max_body_size ${builtins.toString cfg.upload_max_filesize}M;
             '';
           };
           "~ [^/]\.php(/|$)" = {
              return = "403";
           };
-          "/uploads" = {
+          "/uploads/" = {
             alias = cfg.settings.LYCHEE_UPLOADS;
           };
         };
@@ -208,22 +211,27 @@ in
       };
     };
     systemd.tmpfiles.rules = [
-      "d ${cfg.stateDirectory} 0750 ${cfg.user} ${cfg.services.nginx.group}"
-      "d ${cfg.settings.LYCHEE_UPLOADS} 0750 ${cfg.user} ${config.services.nginx.group}"
+      "d ${cfg.stateDirectory} 0750 ${cfg.user} ${config.services.nginx.group}"
+      "d ${cfg.stateDirectory}/www 0750 ${cfg.user} ${config.services.nginx.group}"
+      "C ${cfg.stateDirectory}/public - ${cfg.user} ${config.services.nginx.group} - ${src}/public"
+      "Z ${cfg.stateDirectory}/public 0750 ${cfg.user} ${config.services.nginx.group} - -"
+      "C ${cfg.stateDirectory}/storage - ${cfg.user} ${config.services.nginx.group} - ${src}/storage"
+      "Z ${cfg.stateDirectory}/storage 0750 ${cfg.user} ${config.services.nginx.group} - -"
+      "C ${cfg.settings.LYCHEE_UPLOADS} - ${cfg.user} ${config.services.nginx.group} - ${src}/public/uploads"
+      "Z ${cfg.settings.LYCHEE_UPLOADS} 0750 ${cfg.user} ${config.services.nginx.group} - -"
       "f ${cfg.settings.DB_DATABASE} 0750 ${cfg.user} ${cfg.user}"
     ];
     systemd.services."lychee-install" = {
       description = "Lychee one shot service to install php source into stateDirectory";
-      wantedBy = [ "php-fpm-${cfg.website}.unit" ];
+      wantedBy = [ "phpfpm-${cfg.website}.service" ];
       script = ''
-        ln -s ${src}/* ${cfg.stateDirectory}
-        ln -s ${src}/public/* ${cfg.stateDirectory}/public
-        exit 0
+        ${pkgs.mount}/bin/mount --bind ${src} ${cfg.stateDirectory}/www/
+        ${pkgs.mount}/bin/mount --bind ${cfg.stateDirectory}/public/ ${cfg.stateDirectory}/www/public/
+        ${pkgs.mount}/bin/mount --bind ${cfg.stateDirectory}/storage/ ${cfg.stateDirectory}/www/storage/
         '';
       serviceConfig = {
         Type = "oneshot";
-        User = cfg.user;
-        NoNewPriviledges = true;
+        Restart = "on-failure";
       };
     };
     services.phpfpm.pools.${cfg.website} = {
@@ -231,8 +239,8 @@ in
       phpPackage = pkgs.php81.withExtensions ({ enabled, all }:
         enabled ++ [ all.imagick all.bcmath all.mbstring all.gd]);
       phpOptions = ''
-        upload_max_filesize = ${cfg.upload_max_filesize}M
-        post_max_size = ${cfg.post_max_size}M
+        upload_max_filesize = ${builtins.toString cfg.upload_max_filesize}M
+        post_max_size = ${builtins.toString cfg.post_max_size}M
         '';
       settings = {
         "pm" = "dynamic";
@@ -254,6 +262,7 @@ in
       group = cfg.user;
     };
     users.groups.${cfg.user} = { };
-    networking.firewall.allowedTCPPorts = [ 80 443 ];
+    #networking.firewall.allowedTCPPorts = [ 80 443 ];
   };
 }
+
